0426e63aa9e058ef59b899c767054bf995c15e98
paranoidb <alexandr8558@gmail.com>
2020-06-18 23:42:20 +0300

    bugs fixed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,9 @@ import { FooterContainer, Link } from './styles';
 const Footer = () => {
   return (
     <FooterContainer>
-      <Link to="/">Magic 8 ball</Link>
+      <Link exact to="/">
+        Magic 8 ball
+      </Link>
       <Link to="/prediction">Предсказания</Link>
       <Link to="/hat">Шапка</Link>
     </FooterContainer>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Footer from '../components/Footer';
-import crystalBallScreen from '../screens/CrystalBallScreen';
+import CrystalBallScreen from '../screens/CrystalBallScreen';
 import PredictionScreen from '../screens/PredictionScreen';
 import HatScreen from '../screens/HatScreen';
 
 const Router = () => {
   return (
     <HashRouter>
-      <Route exact path="/" component={crystalBallScreen} />
+      <Route exact path="/" component={CrystalBallScreen} />
       <Route path="/prediction" component={PredictionScreen} />
       <Route path="/hat" component={HatScreen} />
       <Footer />
