db79fca6ed4688f84d9fa8e92253976c1842fa97
paranoidb <alexandr8558@gmail.com>
2020-06-18 22:25:22 +0300

    Added redirect from crystalBall

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import crystalBallScreen from '../screens/CrystalBallScreen';
 import PredictionScreen from '../screens/PredictionScreen';
@@ -8,7 +8,8 @@ import HatScreen from '../screens/HatScreen';
 const Router = () => {
   return (
     <HashRouter>
-      <Route exact path="/crystalBall" component={crystalBallScreen} />
+      <Redirect to="/crystalBall" />
+      <Route path="/crystalBall" component={crystalBallScreen} />
       <Route path="/prediction" component={PredictionScreen} />
       <Route path="/hat" component={HatScreen} />
       <Footer />
